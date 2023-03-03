68306d9bf5fbf680590a938169a7dbc0f408c32c
JakeBritten <jacobbritten@outlook.com>
2023-03-03 18:44:24 -0500

    practice

---
+++ naming test.py
@@ -0,0 +1,7 @@
+# Declare variables for a person's name, age, and favorite color
+name = "Alice"
+age = 30
+color = "blue"
+
+# Print a message to the user that includes their name, age, and favorite color
+print(f"Hello, {name}! You are {age} years old and your favorite color is {color}.")
