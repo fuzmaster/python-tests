3a38af14636c39831709242dcfb8d8798e317300
JakeBritten <jacobbritten@outlook.com>
2023-03-05 16:39:56 -0500

    getting starting with the bank code

--- banktest.py
+++ banktest.py
@@ -1,16 +1,19 @@
-def main():
-    # Prompt the user to enter the credit card debt
-    while True:
-        try:
-            debt = float(input("Enter your credit card debt: ")) # Get the user's credit card debt as a float
-            if debt <= 0:
-                raise ValueError
-            break
-        except ValueError:
-            print("Invalid input. Please enter a positive number for your credit card debt.")
+# Prompt the user to enter the bank name
+bank_name = input("Enter the name of your bank: ")
 
-    # Print the credit card debt to the user
-    print(f"Your credit card debt is {debt}.")
+# Prompt the user to enter the credit card debt limit and current debt
+debt_limit = float(input("Enter credit card debt limit: "))
+debt_current = float(input("Enter credit card debt: "))
 
-if __name__ == '__main__':
-    main()
+# Prompt the user to enter the APR as a float
+apr = float(input("Enter the APR (Annual Percentage Rate) as a decimal: "))
+
+# Calculate the difference between the debt limit and current debt
+debt_available = debt_limit - debt_current
+
+# Output the results to the user
+print(f"Your bank name is {bank_name}.")
+print(f"Your credit card debt limit is {debt_limit:.2f}.")
+print(f"Your current credit card debt is {debt_current:.2f}.")
+print(f"Your APR is {apr:.2%}.")
+print(f"You have {debt_available:.2f} of available credit.")
