fe7dec6a1064f1585b914e0b12f10c2d564df21d
JakeBritten <jacobbritten@outlook.com>
2023-03-06 18:20:11 -0500

    getting starting with the bank code

--- banktest.py
+++ banktest.py
@@ -1,19 +1,22 @@
+# Prompt the user to enter the bank name, credit card debt limit, current debt, and APR
+bank_info = []
+
 # Prompt the user to enter the bank name
-bank_name = input("Enter the name of your bank: ")
+bank_info.append(input("Enter the name of your bank: "))
 
 # Prompt the user to enter the credit card debt limit and current debt
-debt_limit = float(input("Enter credit card debt limit: "))
-debt_current = float(input("Enter credit card debt: "))
+bank_info.append(float(input("Enter credit card debt limit: ")))
+bank_info.append(float(input("Enter credit card debt: ")))
 
-# Prompt the user to enter the APR as a float
-apr = float(input("Enter the APR (Annual Percentage Rate) as a decimal: "))
+# Prompt the user to enter the APR as a decimal with a decimal point
+bank_info.append(float(input("Enter the APR (Annual Percentage Rate) as a decimal (e.g., 0.15 for 15%): ")))
 
 # Calculate the difference between the debt limit and current debt
-debt_available = debt_limit - debt_current
+debt_available = bank_info[1] - bank_info[2]
 
 # Output the results to the user
-print(f"Your bank name is {bank_name}.")
-print(f"Your credit card debt limit is {debt_limit:.2f}.")
-print(f"Your current credit card debt is {debt_current:.2f}.")
-print(f"Your APR is {apr:.2%}.")
+print(f"Your bank name is {bank_info[0]}.")
+print(f"Your credit card debt limit is {bank_info[1]:.2f}.")
+print(f"Your current credit card debt is {bank_info[2]:.2f}.")
+print(f"Your APR is {bank_info[3]:.2%}.")
 print(f"You have {debt_available:.2f} of available credit.")
