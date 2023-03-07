105dd7ff0b1e0b360d2de0d6d84d39464c8ad5c7
JakeBritten <jacobbritten@outlook.com>
2023-03-07 13:34:23 -0500

    enter into an array one bank

--- banktest.py
+++ banktest.py
@@ -1,18 +1,24 @@
 # Prompt the user to enter the bank name, credit card debt limit, current debt, and APR
 bank_info = []
-
-# Prompt the user to enter the bank name
 bank_info.append(input("Enter the name of your bank: "))
-
-# Prompt the user to enter the credit card debt limit and current debt
 bank_info.append(float(input("Enter credit card debt limit: ")))
 bank_info.append(float(input("Enter credit card debt: ")))
-
-# Prompt the user to enter the APR as a decimal with a decimal point
 bank_info.append(float(input("Enter the APR (Annual Percentage Rate) as a decimal (e.g., 0.15 for 15%): ")))
 
 # Calculate the difference between the debt limit and current debt
 debt_available = bank_info[1] - bank_info[2]
+
+# Calculate the monthly interest rate
+monthly_rate = bank_info[3] / 12
+
+# Calculate the minimum monthly payment needed to pay off the debt in 12 months
+monthly_payment = (bank_info[2] * monthly_rate) / (1 - (1 + monthly_rate)**-12)
+
+# Check if the monthly payment is less than the debt available
+if monthly_payment <= debt_available:
+    print("You can pay off your debt in 12 months.")
+else:
+    print(f"To pay off your debt in 12 months, you need to make a monthly payment of {monthly_payment:.2f}.")
 
 # Output the results to the user
 print(f"Your bank name is {bank_info[0]}.")
